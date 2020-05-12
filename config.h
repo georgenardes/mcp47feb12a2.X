06bd95b92391be45d8d81bdce8556f072c2b22b7
georgenardes <georgenardes@gmail.com>
2020-05-12 12:49:33 -0300

    init

---
+++ config.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   config.h
+ * Author: George & Diogo
+ *
+ * Created on 2 de Maio de 2020, 10:34
+ */
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#include <pic18f4520.h>
+#pragma config MCLRE = ON, WDT = OFF, OSC = HS
+
+#define _XTAL_FREQ 16000000
+
+#endif	/* CONFIG_H */
+
---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   main.c
+ * Author: George & Diogo
+ *
+ * Created on 12 de Maio de 2020, 12:37
+ */
+
+
+#include <xc.h>
+#include "config.h"
+
+void main(void) {
+    
+}
